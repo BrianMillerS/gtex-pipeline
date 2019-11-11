2a7425c685897eff77a7a761d60c70878534643e
Nicole Rockweiler <nrockweiler@wustl.edu>
2019-11-11 11:29:50 -0600

    Update paths; stop script if subprocess commands return a non-zero exit code in run_SamToFastq.py

--- rnaseq/src/run_SamToFastq.py
+++ rnaseq/src/run_SamToFastq.py
@@ -7,6 +7,7 @@ import struct
 import subprocess
 from datetime import datetime
 import contextlib
+import sys
 
 @contextlib.contextmanager
 def cd(cd_path):
@@ -21,7 +22,8 @@ parser.add_argument('bam_file', type=str, help='BAM file')
 parser.add_argument('-p', '--prefix', type=str, default='Reads', help='Prefix for output files; usually <sample_id>')
 parser.add_argument('-o', '--output_dir', default=os.getcwd(), help='Directory to which FASTQs will be written')
 parser.add_argument('-m', '--memory', default='8', type=str, help='Memory, in GB')
-parser.add_argument('--jar', default='/opt/picard-tools/picard.jar', help='Path to Picard jar')
+parser.add_argument('--java', default='/opt/apps/java/1.8.0_31/bin/java', help='Path to java')
+parser.add_argument('--jar', default='/opt/apps/picard-tools/2.9.4/picard.jar', help='Path to Picard jar')
 parser.add_argument('--gzip', type=str.lower, default='1', help='gzip compression level for FASTQs; see "man gzip"')
 parser.add_argument('--include_non_pf_reads', type=str.lower, choices=['true', 'false'], default='true', help='Sets INCLUDE_NON_PF_READS option (PF: passed filtering). SamToFastq default: false')
 parser.add_argument('--include_non_primary_alignments', type=str.lower, choices=['true', 'false'], default='false', help='Sets INCLUDE_NON_PRIMARY_ALIGNMENTS option. SamToFastq default: false')
@@ -46,10 +48,15 @@ with cd(args.output_dir):
     subprocess.check_call('gzip -'+args.gzip+' -c < read0_pipe > '+fastq0+' &', shell=True)
 
     # SamToFastq (write to pipes)
-    subprocess.check_call('java -jar -Xmx'+args.memory+'g '+args.jar+' SamToFastq INPUT='+args.bam_file\
+    try:
+        subprocess.check_call(args.java + ' -jar -Xmx'+args.memory+'g '+args.jar+' SamToFastq INPUT='+args.bam_file\
         +' INCLUDE_NON_PF_READS='+args.include_non_pf_reads\
         +' INCLUDE_NON_PRIMARY_ALIGNMENTS='+args.include_non_primary_alignments\
         +' VALIDATION_STRINGENCY=SILENT FASTQ=read1_pipe SECOND_END_FASTQ=read2_pipe UNPAIRED_FASTQ=read0_pipe', shell=True)
+    except subprocess.CalledProcessError as e:
+        print("ERROR: command failed for following reason: %s" % (e), file=sys.stderr)
+        exit(1)
+
 
     # Delete named pipes
     subprocess.check_call('rm read1_pipe read2_pipe read0_pipe', shell=True)
